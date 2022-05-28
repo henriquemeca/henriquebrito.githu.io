d544f75f5f26335c1f83cd7da23bb113d48152f5
312 <3213@eewq.com>
2022-05-28 12:34:59 -0300

    removing .env

--- frontend_react/src/container/About/About.jsx
+++ frontend_react/src/container/About/About.jsx
@@ -19,7 +19,7 @@ const About = () => {
   return (
     <>
       <h2 className="head-text">I Know that <span>Good Design</span> <br />means  <span>Good Business</span></h2>
-
+      <h2>{abouts.title}oi</h2>
       <div className="app__profiles">
         {abouts.map((about, index) => (
           <motion.div
