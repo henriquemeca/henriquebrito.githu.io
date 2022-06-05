491254695d7d643c990683b01df96bd1974c65bf
312 <henriquebritos@hotmail.com>
2022-06-05 17:48:17 -0300

    Update SocialMedia.jsx

--- frontend_react/src/components/SocialMedia.jsx
+++ frontend_react/src/components/SocialMedia.jsx
@@ -1,18 +1,19 @@
 import React from 'react';
-import { BsTwitter, BsInstagram } from 'react-icons/bs';
-import { FaFacebookF } from 'react-icons/fa';
+import { BsGithub, BsLinkedin } from 'react-icons/bs';
+// import { FaFacebookF } from 'react-icons/fa';
 
 const SocialMedia = () => (
-  <div className="app__social" style={{position:'fixed',bottom: '0',left: '0',zIndex:2}}>
-    <div>
-      <BsTwitter />
-    </div>
-    <div>
-      <FaFacebookF />
-    </div>
-    <div>
-      <BsInstagram />
-    </div>
+  <div className="app__social" style={{position:'fixed',bottom: '5%',left: '0',zIndex:2}}>
+    <a href='https://github.com/henriquemeca' target={'_blank'} rel="noopener noreferrer">
+      <div>
+        <BsGithub />
+      </div>
+     </a>
+    <a href='https://www.linkedin.com/in/henriquebritosilva/' target={'_blank'} rel="noopener noreferrer">
+      <div>
+       <BsLinkedin />
+      </div>
+    </a>
   </div>
 );
 
