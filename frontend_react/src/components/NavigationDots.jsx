7d900f07abfc4a14b660b58b1bd7358e79909d18
312 <henriquebritos@hotmail.com>
2022-06-05 18:19:25 -0300

    Navigation dots bug

--- frontend_react/src/components/NavigationDots.jsx
+++ frontend_react/src/components/NavigationDots.jsx
@@ -5,7 +5,7 @@ import React from 'react';
 
 const NavigationDots = ({ active }) => (
   <div className="app__navigation" style={{position:'fixed',bottom: '35%',right: '0',zIndex:2}}>
-    {['home', 'about', 'work', 'skills', 'testimonial', 'contact'].map((item, index) => (
+    {['home', 'about', 'work', 'skills', 'contact'].map((item, index) => (
       <a
         href={`#${item}`}
         key={item + index}
